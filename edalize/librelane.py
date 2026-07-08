5f4746e1aba4f72688ce83ea603a31d3318ab077
nathanchandanson <54677494+nathanchandanson@users.noreply.github.com>
2026-07-08 14:05:35 +0200

    fix: support vlogdefine and vlogparam

--- edalize/librelane.py
+++ edalize/librelane.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 class Librelane(Edatool):
 
-    argtypes = []
+    argtypes = ["vlogdefine", "vlogparam"]
 
     @classmethod
     def get_doc(cls, api_ver):
